9f01f3032688fba9d20589ddf6aa9ca59fcbc746
Naren <nroh555@aucklanduni.ac.nz>
2022-05-23 14:40:13 +1200

    Fix compilation errors for Task 1

--- src/main/java/nz/ac/auckland/se281/a4/ds/NodesStackAndQueue.java
+++ src/main/java/nz/ac/auckland/se281/a4/ds/NodesStackAndQueue.java
@@ -23,56 +23,85 @@ public class NodesStackAndQueue<T> {
 	 * @return true if the stack / queue is empty
 	 */
 	public boolean isEmpty() {
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+		return (head == null);
 	}
 
 	/**
-	 * Push operation refers to inserting an element in the Top of the stack.
-	 * TODO: Complete this method
+	 * Push operation refers to inserting an element in the Top of the stack. TODO:
+	 * Complete this method
 	 * 
-	 * @param element
-	 *            the element to be "pushed"
+	 * @param element the element to be "pushed"
 	 */
 	public void push(T element) {
-		Node n = new Node(element);
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
-
+		// Top of the stack is the head
+		Node<T> n = new Node<T>(element);
+		n.setNext(head);
+		head = n;
 	}
 
 	/**
-	 * pop an element from the top of the stack (removes and returns the tope
-	 * element)
-	 * TODO: Complete this method (Note: You may have to change the return type)
+	 * pop an element from the top of the stack (removes and returns the top
+	 * element) TODO: Complete this method (Note: You may have to change the return
+	 * type)
 	 * 
 	 * @return object of the top element
-	 * @throws EmptyStackException
-	 *             if the stack is empty
+	 * @throws EmptyStackException if the stack is empty
 	 */
-	public Node pop() throws EmptyStackException {
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+	public T pop() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		else {
+			// A temporary variable to store the value of the current head before being
+			// removed
+			Node<T> temp = head;
+			head = head.getNext();
+			return temp.getValue();
+		}
 	}
 
 	/**
-	 * get the element from the top of the stack without removing it
-	 * TODO: Complete this method (Note: You may have to change the return type)
+	 * get the element from the top of the stack without removing it TODO: Complete
+	 * this method (Note: You may have to change the return type)
 	 *
 	 * @return the value of the top element
-	 * @throws EmptyStackException
-	 *             if the stack is empty
+	 * @throws EmptyStackException if the stack is empty
 	 */
-	public Node peek() throws EmptyStackException {
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+	public T peek() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+
+		else {
+			return head.getValue();
+		}
+
 	}
 
 	/**
-	 * append an element at the end of the queue
-	 * TODO: Complete this method
+	 * append an element at the end of the queue TODO: Complete this method
 	 *
-	 * @param element
-	 *            the element to be appended
+	 * @param element the element to be appended
 	 */
 	public void append(T element) {
-		Node n = new Node(element);
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+		Node<T> n = new Node<T>(element);
+
+		// Checks if there is an element in the list
+		if (isEmpty()) {
+			head = n;
+			return;
+		}
+
+		Node<T> last = head;
+		Node<T> temp = last;
+		while (last != null) {
+			// A temporary node to store the last node before it is null
+			temp = last;
+			last = last.getNext();
+		}
+
+		// Appends the next node of the last node
+		temp.setNext(n);
 	}
 }
